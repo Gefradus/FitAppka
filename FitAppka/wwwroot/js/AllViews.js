832ba981f2042ec39677b0f69689914b8bcca4e3
DratwaK <krzysztof.dratwa@ttsw.com.pl>
2020-08-21 14:27:51 +0200

    o lol

--- FitAppka/wwwroot/js/AllViews.js
+++ FitAppka/wwwroot/js/AllViews.js
@@ -1,8 +1,8 @@
-﻿function kolorujDrugiWiersz() {
+﻿function kolorujCoDrugiWiersz() {
     var wszystkie = document.getElementsByClassName("wieeersz");
     for (var i = 0; i < wszystkie.length; i++) {
         if (i % 2 == 0) {
-            wszystkie[i].style.backgroundColor = "rgb(243, 243, 243)";
+            wszystkie[i].style.backgroundColor = "rgb(239, 239, 239)";
         }
     }
 }
