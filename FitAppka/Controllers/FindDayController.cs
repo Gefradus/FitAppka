5140cedaae97645d78271c9770d95f39f4936e5c
Gefradus <fader12345@gmail.com>
2020-08-31 22:38:51 +0200

    aaaaa

--- FitAppka/Controllers/FindDayController.cs
+++ FitAppka/Controllers/FindDayController.cs
@@ -1,5 +1,4 @@
-﻿using FitAppka.Models;
-using FitAppka.Repository;
+﻿using FitAppka.Repository;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
--- FitAppka/Controllers/TrainingController.cs
+++ FitAppka/Controllers/TrainingController.cs
@@ -21,7 +21,10 @@ namespace FitAppka.Controllers
         private readonly IStrengthTrainingRepository _strengthTrainingRepository;
         private readonly FitAppContext _context;
 
-        public TrainingController(FitAppContext context, IDayRepository dayRepository, IClientRepository clientRepository, ICardioTrainingTypeRepository cardioTypeRepository, ICardioTrainingRepository cardioRepository, IStrengthTrainingTypeRepository strengthTrainingTypeRepository, IStrengthTrainingRepository strengthTrainingRepository)
+        public TrainingController(FitAppContext context, IDayRepository dayRepository, 
+            IClientRepository clientRepository, ICardioTrainingTypeRepository cardioTypeRepository, 
+            ICardioTrainingRepository cardioRepository, IStrengthTrainingTypeRepository strengthTrainingTypeRepository, 
+            IStrengthTrainingRepository strengthTrainingRepository) 
         {
             _cardioTypeRepository = cardioTypeRepository;
             _cardioRepository = cardioRepository;
@@ -33,8 +36,7 @@ namespace FitAppka.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> TrainingPanel(int dayID)
-        {
+        public async Task<IActionResult> TrainingPanel(int dayID) {
             dayID = GiveTodayIfDayNotChosen(dayID);
             ViewData["day"] = GetDayDateById(dayID);
             ViewData["dayID"] = dayID;
@@ -43,47 +45,39 @@ namespace FitAppka.Controllers
             ViewData["cardioTime"] = CardioTimeInDay(dayID);
             ViewData["kcalTarget"] = GetKcalBurnedGoalInDay(dayID);
             ViewData["timeTarget"] = GetTrainingTimeGoalInDay(dayID);
-            ViewData["strengthTrainings"] = _strengthTrainingRepository.GetAllStrengthTrainings();
+            ViewData["strengthTrainings"] = _context.StrengthTraining.Include(s => s.StrengthTrainingType).Include(s => s.Day).ToList();
 
             return View(await _context.CardioTraining.Include(c => c.CardioTrainingType).Include(c => c.Day).ToListAsync());
         }
 
 
         [HttpGet]
-        public IActionResult ChangeDay(string day)
-        {
+        public IActionResult ChangeDay(string day) {
             return RedirectToAction(nameof(TrainingPanel), new { dayID = GetSelectedDay(Convert.ToDateTime(day)) });
         }
 
-        private int GetKcalBurnedGoalInDay(int dayID)
-        {
+        private int GetKcalBurnedGoalInDay(int dayID) {
             int? kcalBurnedGoal = _dayRepository.GetDay(dayID).KcalBurnedGoal;
-            if(kcalBurnedGoal != null)
-            {
+            if(kcalBurnedGoal != null) {
                 return (int) kcalBurnedGoal;
             }
-            else
-            {
+            else {
                 return 0;
             }
 
         }
 
-        private int GetTrainingTimeGoalInDay(int dayID)
-        {
-            int? celMinut = _dayRepository.GetDay(dayID).TrainingTimeGoal;
-            if (celMinut != null)
-            {
-                return (int)celMinut;
+        private int GetTrainingTimeGoalInDay(int dayID) {
+            int? timeGoal = _dayRepository.GetDay(dayID).TrainingTimeGoal;
+            if (timeGoal != null) {
+                return (int)timeGoal;
             }
-            else
-            {
+            else {
                 return 0;
             }
         }
 
-        private int CaloriesBurnedInDay(int dayID)
-        {
+        private int CaloriesBurnedInDay(int dayID) {
             int? kcal = 0;
             foreach (CardioTraining cardio in _cardioRepository.GetAllCardioTrainings().Where(t => t.DayId.Equals(dayID))) {
                 kcal += cardio.CaloriesBurned;
@@ -92,20 +86,16 @@ namespace FitAppka.Controllers
             return (int) kcal;
         }
 
-        private int GiveTodayIfDayNotChosen(int dayID)
-        {
-            if(dayID == 0)
-            {
+        private int GiveTodayIfDayNotChosen(int dayID) {
+            if(dayID == 0) {
                 return GetTodayID();
             }
-            else
-            {
+            else {
                 return dayID;
             }
         }
 
-        private int CardioTimeInDay(int dayID)
-        {
+        private int CardioTimeInDay(int dayID) {
             int? time = 0;
             foreach (CardioTraining cardio in _cardioRepository.GetAllCardioTrainings().Where(t => t.DayId.Equals(dayID))) {
                 time += cardio.TimeInMinutes;
@@ -115,20 +105,16 @@ namespace FitAppka.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Search(string search, int dayID)
-        {
+        public async Task<IActionResult> Search(string search, int dayID) {
             ViewData["dayID"] = dayID;
             CheckIfThisWasSearchedFor(search);
             return View(await _context.CardioTrainingType.Where(c => c.TrainingName.Contains(search)).ToListAsync());
         }
 
         [HttpPost]
-        public IActionResult Search(int cardioTypeId, int dayID, int timeInMinutes, int burnedKcal)
-        {
-            try
-            {
-                _cardioRepository.Add(new CardioTraining()
-                {
+        public IActionResult Search(int cardioTypeId, int dayID, int timeInMinutes, int burnedKcal) {
+            try {
+                _cardioRepository.Add(new CardioTraining() {
                     DayId = dayID,
                     TimeInMinutes = timeInMinutes,
                     CardioTrainingTypeId = cardioTypeId,
@@ -140,17 +126,14 @@ namespace FitAppka.Controllers
         }
 
         [HttpPost]
-        public JsonResult DeleteCardio(int cardioID)
-        {
+        public JsonResult DeleteCardio(int cardioID) {
             _cardioRepository.Delete(cardioID);
             return Json(false);
         }
 
         [HttpPost]
-        public IActionResult AddCardioType(int dayID, string name, int kcalPerMin)
-        {
-            _cardioTypeRepository.Add(new CardioTrainingType
-            {
+        public IActionResult AddCardioType(int dayID, string name, int kcalPerMin) {
+            _cardioTypeRepository.Add(new CardioTrainingType {
                 TrainingName = name,
                 KcalPerMin = kcalPerMin
             });
@@ -159,20 +142,17 @@ namespace FitAppka.Controllers
         }
 
 
-        private int GetSelectedDay(DateTime day)
-        {
+        private int GetSelectedDay(DateTime day) {
             AddDayIfNotExists(day);
             return GetClientDayIDByDate(day);
         }
 
-        private void AddDayIfNotExists(DateTime day)
-        {
+        private void AddDayIfNotExists(DateTime day) {
             var client = _clientRepository.GetClientById(GetLoggedInClientID());
             int count = _context.Day.Count(dz => dz.Date == day && dz.ClientId == client.ClientId);
             if (count == 0)
             {
-                _dayRepository.Add(new Day()
-                {
+                _dayRepository.Add(new Day() {
                     Date = day,
                     ClientId = client.ClientId,
                     Breakfast = client.Breakfast,
@@ -191,23 +171,20 @@ namespace FitAppka.Controllers
         }
 
 
-        private int GetClientDayIDByDate(DateTime day)
-        {
+        private int GetClientDayIDByDate(DateTime day) {
             AddDayIfNotExists(day);
             return _dayRepository.GetClientDayByDate(day, GetLoggedInClientID()).DayId;
         }
 
-        private int GetTodayID()
-        {
+        private int GetTodayID() {
             return GetSelectedDay(DateTime.Now);
         }
 
-        private string GetDayDateById(int dayID)
-        {
+        private string GetDayDateById(int dayID) {
             return _dayRepository.GetDayDateTime(dayID).Date.ToString("dd.MM.yyyy");
         }
 
-        private void CheckIfThisWasSearchedFor(string search){
+        private void CheckIfThisWasSearchedFor(string search) {
             ViewData["wasSearched"] = false;
         
             if (search != null)
