78884b9bf4c54caf77985fe1b9d79454b856598c
Gefradus <fader12345@gmail.com>
2020-09-23 22:56:34 +0200

    poprawki

---
+++ FitAppka/wwwroot/js/Datepicker.js
@@ -0,0 +1,23 @@
+﻿function createDatePicker(selector) {
+    var $j = jQuery.noConflict();
+    $j(selector).datepicker({
+        minDate: new Date(1900, 1, 1),
+        dateFormat: 'dd.mm.yy',
+        monthNames: ["Styczeń", "Luty", "Marzec", "Kwiecień",
+            "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień",
+            "Październik", "Listopad", "Grudzień"],
+        monthNamesShort: ["Sty", "Lut", "Mar", "Kwi",
+            "Maj", "Cze", "Lip", "Sie", "Wrz",
+            "Paź", "Lis", "Gru"],
+        dayNames: ['Niedziela', 'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota'],
+        dayNamesShort: ['Ndz', 'Pon', 'Wto', 'Śro', 'Czw', 'Pt', 'Sob'],
+        dayNamesMin: ['Nd', 'Pn', 'Wt', 'Śr', 'Cz', 'Pt', 'Sb'],
+        changeYear: true,
+        changeMonth: true
+    });
+}
+
+function createDatePickers() {
+    createDatePicker("#datepickerFrom");
+    createDatePicker("#datepickerTo");
+}
--- FitAppka/wwwroot/js/ProgressMonitoring.js
+++ FitAppka/wwwroot/js/ProgressMonitoring.js
@@ -1,39 +1,28 @@
 ﻿function createChart() {
+    Chart.defaults.global.defaultFontColor = 'rgb(255, 255, 255)';
     var ctx = document.getElementById('myChart').getContext('2d');
     var myChart = new Chart(ctx, {
-        type: 'bar',
+        type: 'line',
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota', 'Niedziela'],
             datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
+                label: 'Pomiar wagi',
+                data: [90, 89, 89, 89.5, 88.7, 88],
+                borderColor: 'rgb(3, 202, 86)',
+                backgroundColor: 'rgba(0, 50, 0, 0.0)',
+            },
+
+            
+            ]
         },
         options: {
-            scales: {
+            /*scales: {
                 yAxes: [{
                     ticks: {
                         beginAtZero: true
                     }
                 }]
-            }
+            }*/
         }
     });
 }
