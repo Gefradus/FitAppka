49c13849893ccb9bdee2ff98fd07835e34a80345
Gefradus <fader12345@gmail.com>
2020-09-28 00:22:14 +0200

    MoBByn odwrocony West Coast

--- FitAppka/Models/Entity/Client.cs
+++ FitAppka/Models/Entity/Client.cs
@@ -28,7 +28,7 @@ namespace FitAppka.Models
         
         [StringLength(50)]
         public string Login { get; set; }
-        [Required]
+
         [StringLength(50)]
         public string Password { get; set; }
         [Column("First_name")]
